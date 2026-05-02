92c7574540c4c96888c6e07b08dd66449ef72484
Reverendheat <sharp.brandon@gmail.com>
2026-05-02 10:44:44 -0400

    refactor: move proxy handlers out of main

--- main.go
+++ main.go
@@ -1,11 +1,6 @@
 package main
 
-import (
-	"io"
-	"net"
-	"net/http"
-	"time"
-)
+import "net/http"
 
 func main() {
 	cfg := NewConfig()
@@ -19,79 +14,4 @@ func main() {
 
 	cfg.Logger.Info("turbulence proxy listening", "port", cfg.Listen)
 	cfg.Logger.Error("server error", "error", server.ListenAndServe())
-}
-
-func handleProxy(w http.ResponseWriter, r *http.Request, cfg Config) {
-	if shouldDrop(cfg) {
-		cfg.Logger.Info("dropping", "method", r.Method, "host", r.Host)
-		http.Error(w, "turbulence proxy dropped request", http.StatusServiceUnavailable)
-		return
-	}
-
-	injectDelay(cfg)
-
-	if r.Method == http.MethodConnect {
-		handleConnect(w, r, cfg)
-		return
-	}
-
-	handleHTTP(w, r, cfg)
-}
-
-func handleHTTP(w http.ResponseWriter, r *http.Request, cfg Config) {
-	cfg.Logger.Info("http info", "method", r.Method, "url", r.URL.String())
-
-	r.RequestURI = ""
-
-	resp, err := http.DefaultTransport.RoundTrip(r)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadGateway)
-		return
-	}
-	defer resp.Body.Close()
-
-	copyHeader(w.Header(), resp.Header)
-	w.WriteHeader(resp.StatusCode)
-
-	_, _ = io.Copy(w, resp.Body)
-}
-
-func handleConnect(w http.ResponseWriter, r *http.Request, cfg Config) {
-	cfg.Logger.Info("connect info", "host", r.Host)
-
-	upstream, err := net.DialTimeout("tcp", r.Host, 10*time.Second)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusServiceUnavailable)
-		return
-	}
-	defer upstream.Close()
-
-	hijacker, ok := w.(http.Hijacker)
-	if !ok {
-		http.Error(w, "connection hijacking unsupported", http.StatusInternalServerError)
-		return
-	}
-
-	client, _, err := hijacker.Hijack()
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusServiceUnavailable)
-		return
-	}
-	defer client.Close()
-
-	_, _ = client.Write([]byte("HTTP/1.1 200 Connection Established\r\n\r\n"))
-
-	done := make(chan struct{}, 2)
-
-	go func() {
-		_, _ = io.Copy(upstream, client)
-		done <- struct{}{}
-	}()
-
-	go func() {
-		_, _ = io.Copy(client, upstream)
-		done <- struct{}{}
-	}()
-
-	<-done
 }
